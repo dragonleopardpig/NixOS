201f23a927581cdc3ad13f0f949d6fbf79d750e2
dragonleopardpig <dragonleopardpig@gmail.com>
2024-04-02 13:13:38 +0800

    update emacs/init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,7 @@
 ;; Save this file in ~/.emacs.d/
 
-(load  "/home/plchu/emacs/user/preload.el")
-(load  "/home/plchu/scimax/init.el")
-(load  "/home/plchu/emacs/user/user.el")
+(load  "/home/thinky/emacs/user/preload.el")
+;; (load  "/home/thinky/mountdir/emacs/scimax/init.el")
+(load  "/home/thinky/Downloads/scimax/init.el")
+(load  "/home/thinky/emacs/user/user.el")
+(load  "/home/thinky/emacs/user/themes.el")
