813dc9ef983f6989c537624dce5a0a4f046803bc
dragonleopardpig <dragonleopardpig@gmail.com>
2025-10-11 20:23:09 +0800

    nixos

--- emacs/init.el
+++ emacs/init.el
@@ -1,16 +1,15 @@
 ;; Save this file in ~/.emacs.d/
 
-(load  "/home/thinky/emacs/user/preload.el")
-;; (load  "/home/thinky/mountdir/emacs/scimax/init.el")
+(load  "/home/thinky/Downloads/NixOS/emacs/user/preload.el")
 (load  "/home/thinky/Downloads/scimax/init.el")
-(load  "/home/thinky/emacs/user/user.el")
-;; (load  "/home/thinky/emacs/user/themes.el")
+(load  "/home/thinky/Downloads/NixOS/emacs/user/user.el")
+(load  "/home/thinky/Downloads/NixOS/emacs/user/themes.el")
 
 ;; Add your custom lisp directory to load-path
-(add-to-list 'load-path "~/NixOS/emacs/user/custom/")
+(add-to-list 'load-path "~Downloads/NixOS/emacs/user/custom/")
 
 ;; Load your custom files
 ;; (load "my-custom-file.el")
 ;; (load "another-custom-file.el")
 ;; Or, if you have many, you can iterate and load them
-(mapc 'load (file-expand-wildcards "~/NixOS/emacs/user/custom/*.el"))
+(mapc 'load (file-expand-wildcards "~Downloads/NixOS/emacs/user/custom/*.el"))
