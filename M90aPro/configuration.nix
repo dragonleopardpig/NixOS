0fd152941ffa5f060d1a669e921d34c93eeacb23
dragonleopardpig <dragonleopardpig@gmail.com>
2025-10-14 21:24:35 +0800

    boot silence

--- M90aPro/configuration.nix
+++ M90aPro/configuration.nix
@@ -23,10 +23,30 @@
     boot.loader.efi.efiSysMountPoint = "/boot";
 
     boot.loader.grub2-theme = {
-    enable = true;
-    theme = "stylish";
-    footer = true;
-    customResolution = "2560x1440";  # Optional: Set a custom resolution
+      enable = true;
+      theme = "stylish";
+      footer = true;
+      customResolution = "2560x1440";  # Optional: Set a custom resolution
+    };
+
+    boot = {
+      # silence first boot output
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      initrd.systemd.enable = true;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "intremap=on"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+
+      # plymouth, showing after LUKS unlock
+      plymouth.enable = true;
+      plymouth.font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
+      plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
     };
     
   boot.initrd.luks.devices."luks-51c7cb8c-d514-40e1-8286-0185987e196c".device = "/dev/disk/by-uuid/51c7cb8c-d514-40e1-8286-0185987e196c";
