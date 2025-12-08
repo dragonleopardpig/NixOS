f7313b35465b6444656628d974cb1af3696f5d7e
installer <installer@gmail.com>
2025-12-08 13:48:32 +0800

    done

---
+++ cpp/cpp.cpp
@@ -0,0 +1,11 @@
+// run "bear -- clang++ cpp.cpp -o cpp"
+// open compile_commands.json, remove duplicated "arguments" section.
+// ammend related field
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello World!";
+  return 0;
+}
